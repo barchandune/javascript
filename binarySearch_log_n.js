64afb4877502ca383b6cf138bacf87f7d93d6a47
Apoorv Khare <apoorv.khare@outlook.com>
2024-06-17 02:16:12 -0700

    Update binarySearch_log_n.js

--- binarySearch_log_n.js
+++ binarySearch_log_n.js
@@ -8,18 +8,17 @@ var searchInsert = function (nums, target) {
     let min = 0;
     let max = nums.length - 1;
     // let mid = 0
-    while (min <=max) {
+    while (min <= max) {
         // mid = mid + (max - min) / 2 // 1 2
 
-        let mid = Math.floor((min, max) / 2)
+        let mid = Math.floor((min + max) / 2); // (min + max) / 2
         if (target > nums[mid]) {
-            min = mid + 1 // 2
+            min = mid + 1
         } else if (target < nums[mid]) {
             max = mid - 1
-        } else if (target === nums[mid]) {
-            return mid
         } else {
-            return min + 1
+            return mid
         }
     }
+    return min
 };
